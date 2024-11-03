4c0f6fb03c76252b56bd1eafd9b5d8b08131943f
Tabish <tabishraj7@gmail.com>
2024-11-03 11:38:15 +0530

    NPM basics and advanced

---
+++ NPM.js
@@ -0,0 +1,40 @@
+// 1.NPM understanding 
+
+// npm - node package manager 
+// npm - nahi pata mujhe 
+
+// node js core mein jo installed aata hai wo kehlaata hai module
+// npm se download karte hai wo hota hai package
+
+// text to speech -> aise application jo visually impaired logo ke liye banana h to ye package h
+
+
+// 2. installing and uninstalling anything basics and advanced 
+
+// install -> npm i packageKaNaam  or npm install packageKaNaam
+// uninstall -> npm uninstall packageKaNaam
+// particular version install -> npm i packageKaNaam@joVersionchahiye
+
+
+// understanding node modules -> isse hamein lena dena nahi h, jo packjage download karte h uska dependency uspe rhta hai that's why wo bhi download ho jata hai
+
+
+// dependencies - packages and packages ki dependencies
+// devdependencies - aise package jo sirf development mein kaam ayega par jab app ban jayega and upload ho jayega tab ham in packages ko use nahi kar rhe honge
+
+
+// scripts - understanding default scripts PATH and custom scripts
+
+// kai baar aap koi script chalate ho and us script main aap likhte ho ye command ->
+
+// npm start
+// npm run dev
+// npm run concurrent
+
+// start and test aise 2 command h jo install karte samay path mein include kar diye jate hai to (npm start) ya (npm test) likhne se run ho jata hai but,
+// agar ham koi apne se command dena chahate hai to usme (run) lagana padta hai
+
+
+// for colourful, we install package -> chalk
+
+console.log("hi, what happen?")
